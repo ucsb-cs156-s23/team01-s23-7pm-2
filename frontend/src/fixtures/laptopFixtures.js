297c519fbc460efd254145b03a46342a4669bfd8
Arik <arikcheslog@gmail.com>
2023-04-20 15:11:10 -0700

    ac - added laptopFixtures

---
+++ frontend/src/fixtures/laptopFixtures.js
@@ -0,0 +1,46 @@
+const laptopFixtures = {
+    oneLaptop:
+    [
+      {
+        "id": 1, // https://www.hp.com/us-en/shop/mdp/envy---omen/omen-16-3074457345617607169--1
+        "name": "OMEN 16t-k000",
+        "cpu": "Intel Core i5-12500H",
+        "gpu": "NVIDIA GeForce RTX 3050 Laptop",
+        "price": 94999,
+        "description": "Cheap but still fast"  
+      }
+    ],
+
+    threeLaptops:
+    [
+        {
+            "id": 2, // https://www.bestbuy.com/site/asus-11-6-laptop-intel-celeron-n4020-4gb-memory-64gb-emmc-star-black/6498805.p?skuId=6498805
+            "name": "ASUS E210",
+            "cpu": "Intel Pentium N4020",
+            "gpu": "Integrated Intel UHD Graphics",
+            "price": 10399,
+            "description": "Extremely cheap and functional"  
+        },
+
+        {
+            "id": 3,  // https://www.dell.com/en-us/shop/dell-laptops/alienware-m18-gaming-laptop/spd/alienware-m18-r1-laptop
+            "name": "Alienware m18",
+            "cpu": "Intel Core i7-13650HX",
+            "gpu": "NVIDIA GeForce RTX 4050 Laptop",
+            "price": 22900,
+            "description": "Extremely fast but expensive"  
+        },
+
+        {
+            "id": 4,  // https://www.walmart.com/ip/Lenovo-Ideapad-3i-14-FHD-Laptop-Intel-Core-i3-1115G4-4GB-128GB-SSD-Windows-11-in-S-Mode-Platinum-Grey-81X700FGUS/125496557?wmlspartner=wlpa&selectedSellerId=0
+            "name": "Lenovo Ideapad 3i",
+            "cpu": "Intel Core i3-1115G4",
+            "gpu": "Integrated Intel UHD Graphics",
+            "price": 199999,
+            "description": "Touchscreen"  
+        },
+        
+    ]
+};
+
+export { laptopFixtures };
